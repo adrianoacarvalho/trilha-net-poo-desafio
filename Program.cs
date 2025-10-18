3adc42684d328bff7170283fdfa062e1c5627e01
Adriano Andrade Carvalho <acarvalho.software@gmail.com>
2025-10-18 14:56:24 -0300

    Implementando a logica

--- Program.cs
+++ Program.cs
@@ -1,3 +1,13 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Iphone:");
+Iphone iphone = new Iphone("123456789", "iPhone 13", "IMEI123456", 128);
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("WhatsApp");  
+
+Console.WriteLine("\nSmartphone Nokia:");
+Nokia nokia = new Nokia("987654321", "Nokia 3310", "IMEI987654", 32);
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("Telegram");   
